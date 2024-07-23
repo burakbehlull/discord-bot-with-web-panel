ca6090acd014028ee01589b91c59a0e02e25f6c9
burakbehlull <burakyabgu@gmail.com>
2024-07-23 23:07:49 +0300

    feat: Create user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,36 @@
-// user model
+import { Schema, model } from "mongoose"
+
+const UserSchema = new Schema({
+    displayName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    lastLoggedIn: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+export default model('User', UserSchema)
