99b092cd0f56920ca1b0dd566cda1f5413692a00
burakbehlull <burakyabgu@gmail.com>
2024-07-23 18:13:21 +0300

    feat: Make basic backend system

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
-const express = require('express')
-require('dotenv').config()
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 
 
 const app = express()
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use(cors({
+    origin: true,
+	credentials: true
+}))
 
 app.get('/',(req,res)=>{
     res.send('hello public bot with web panel!')
